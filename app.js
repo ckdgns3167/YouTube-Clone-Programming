8e10b08f238b15d264e8239e107ba6b1a9a3bce9
newckdgns <newckdgns@naver.com>
2020-08-19 21:44:40 +0900

    MVC 모델링 中 Controller 작업. (필요할 웹 페이지에 대한 각각의 URL 라우팅 작업)

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
+// 사용할 모듈 import 
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import globalRouter from "./routers/globalRouter"
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 
-const app = express(); // express 서버를 생성하여 app이라는 변수에 저장.
-
-const handleHome = (req, res) => res.send("Hello from home.");
-
-const handleProfile = (req, res) => res.send("You are on my profile.");
+const app = express(); // express 서버 객체를 생성하여 app이라는 변수에 저장.
 
 /**
  * Set middleware. (미들웨어 설정: 위치가 중요함. 해당 위치는 Global 영역이기 때문에 모든 페이지 접근 전에 동작됨.)
@@ -21,11 +21,10 @@ app.use(bodyParser.json()); // JSON 형태의 데이터를 읽을 수 있음.
 app.use(bodyParser.urlencoded({ extended: true })); // extended: true 옵션에 의해 qs 모듈을 사용하여 쿼리스트링 파싱 -> req.body에 jSON 형식으로 저장되어 데이터 리딩 가능.
 
 /**
- * Routing web page address (URL로 페이지 접근 가능하게 함.)
+ * Routing.
  */
-app.get("/", handleHome);
-app.get("/profile", handleProfile)
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
