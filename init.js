47eba4792a91828b8a43469029fa26efb946f23e
newckdgns <newckdgns@naver.com>
2020-08-19 19:54:33 +0900

    Update init.js

--- init.js
+++ init.js
@@ -6,6 +6,6 @@ const handleListening = () =>
     console.log(`✅ Listening on: http://localhost:${PORT}`);
 
 /**
- * Receiving user access to the Express Server (사용자의 접근을 기다림.)
+ * Receiving user access to the Express Server (사용자의 접근을 기다림(Listen).)
  */
 app.listen(PORT, handleListening);
