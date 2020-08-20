c984f4fdb59d19649bf9c76bc83a58f557ad82bc
newckdgns <newckdgns@naver.com>
2020-08-20 10:50:09 +0900

    MVC 모델링 中 View작업. (pug 모듈 추가, 필요한 페이지들에 대한 템플릿 생성, 템플릿 랜더링 작업) 여러 곳에서 쓰이는 몇몇 단어를 한 곳에서 관리할 수 있도록 미들웨어 생성.

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import globalRouter from "./routers/globalRouter"
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-import routes from "./routes";
+import globalRouter from "./routers/globalRouter"
 
 const app = express(); // express 서버 객체를 생성하여 app이라는 변수에 저장.
+
 
 /**
  * Set middleware. (미들웨어 설정: 위치가 중요함. 해당 위치는 Global 영역이기 때문에 모든 페이지 접근 전에 동작됨.)
  */
 app.use(helmet()); // 보안
-app.use(morgan("dev")); // 로깅
+app.set("view engine", "pug"); // View template engine 설정
 app.use(cookieParser()); // 사용자 정보를 담고 있는 쿠키를 생성할 수 있음.
 app.use(bodyParser.json()); // JSON 형태의 데이터를 읽을 수 있음.
 app.use(bodyParser.urlencoded({ extended: true })); // extended: true 옵션에 의해 qs 모듈을 사용하여 쿼리스트링 파싱 -> req.body에 jSON 형식으로 저장되어 데이터 리딩 가능.
-
+app.use(morgan("dev")); // 로깅
+app.use(localsMiddleware);
 /**
  * Routing.
  */
---
+++ middlewares.js
@@ -0,0 +1,8 @@
+// 전역적으로(글로벌) 사용할 수 있는 변수를 추가하는 방법.
+import routes from "./routes";
+
+export const localsMiddleware = (req, res, next) => {
+    res.locals.siteName = "WeTube";
+    res.locals.routes = routes;
+    next();
+};
