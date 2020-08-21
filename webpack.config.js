55c9291298fda1213565e71cb9a81d54433eaaae
newckdgns <newckdgns@naver.com>
2020-08-22 02:53:57 +0900

    Update webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.resolve(__dirname, "static");
 
 const config = {
+  devtool: "cheap-module-source-map", // unsafe-eval 관련 오류때문에 추가함.
   entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
