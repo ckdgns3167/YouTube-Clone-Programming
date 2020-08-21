02f1ad77cabbb586d3b67e33e76e892c778587bc
newckdgns <newckdgns@naver.com>
2020-08-21 16:05:47 +0900

    동영상 수정 및 삭제 기능 구현. (사용자 인증은 아직)

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,10 +2,11 @@ import express from "express";
 import routes from "../routes";
 import {
     videoDetail,
-    editVideo,
-    deleteVideo,
     postUpload,
     getUpload,
+    deleteVideo,
+    getEditVideo,
+    postEditVideo
 } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
@@ -15,8 +16,14 @@ const videoRouter = express.Router();
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
+// Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
